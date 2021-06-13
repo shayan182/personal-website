8f92f634c8ef1027f6f817c46b25a59a6cc8cda2
shayan <182shayan@gamail.com>
2021-06-13 11:00:47 +0330

    created mvc project

---
+++ Personal-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Personal_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
